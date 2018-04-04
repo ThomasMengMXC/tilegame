ce02c2f55d366bb3916317e5c6f9501f0a5d9b34
Thomas Meng <thomasmengmxc@gmail.com>
2018-04-04 16:54:44 +1000

    Made the scenes load dynamically and automatically.

--- src/game.c
+++ src/game.c
@@ -1,5 +1,9 @@
 #include <dlfcn.h>
 #include <wordexp.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <theatre/play.h>
 #include <theatre/stage.h>
 #include <theatre/props.h>
@@ -9,18 +13,67 @@
 
 int main(int argc, char **argv) {
 	Stage *stage;
+
+    // pre show setup
 	init_theatre();
+
+    // preparing the stage just before the show
 	stage = init_stage();
 
-	load_scene(stage, "~/.config/tilegame/scenes/overmap.so");
+    // getting the scripts ready from ~/.config/tilegame/scenes
+    char *config = "~/.config/tilegame/scenes";
+    void **handles = load_all_scenes(stage, config);
+
+    // start the show
 	enact_play(stage);
 
+    // free the handles
+    free(handles);
+
+    // clean up the stage
 	free_stage(stage);
+
+    // post show cleanup
 	finale();
 	return 0;
 }
 
-void load_scene(Stage *stage, char *name) {
+void **load_all_scenes(Stage *stage, char *directory) {
+    wordexp_t p;
+	wordexp(directory, &p, 0);
+
+    DIR *dir;
+    struct dirent *ent;
+    int sceneCount = 0;
+    void **handles = NULL;
+    handles = realloc(handles, sizeof(void *) * (sceneCount + 1));
+    if ((dir = opendir(p.we_wordv[0])) != NULL) {
+        while ((ent = readdir (dir)) != NULL) {
+            char *name = malloc(sizeof(char) * (strlen(directory)
+                        + strlen(ent->d_name) + 2));
+            if (ent->d_name[0] != '.') {
+                sprintf(name, "%s/%s", directory, ent->d_name);
+                sceneCount++;
+                handles = realloc(handles, sizeof(void *) * (sceneCount + 1));
+                handles[sceneCount - 1] = load_scene(stage, name);
+            }
+            free(name);
+        }
+        closedir (dir);
+    };
+    handles[sceneCount] = NULL;
+	wordfree(&p);
+    return handles;
+}
+
+void unload_all_scenes(void **handles) {
+    for (int i = 0; handles[i] != NULL; i++) {
+        dlclose(handles[i]);
+    }
+    return;
+}
+
+void *load_scene(Stage *stage, char *name) {
 	void *handle;
     wordexp_t p;
     // evaluate the name
@@ -33,7 +86,7 @@ void load_scene(Stage *stage, char *name) {
     // check if the handle screwed up
 	if (!handle) {
 		fprintf (stderr, "%s\n", dlerror());
-		return 1;
+		exit(1);
 	}
 	dlerror();
 
@@ -47,5 +100,5 @@ void load_scene(Stage *stage, char *name) {
 	exit = (ExitFn) dlsym(handle, "overmap_exit");
     // add the functions to the stage
 	add_scene_to_stage(stage, update, keyboard, entry, exit);
-    return;
+    return handle;
 }
--- src/game.h
+++ src/game.h
@@ -1,6 +1,8 @@
 #ifndef GAME_H
 #define GAME_H
 
-void load_scene(Stage *stage, char *name);
+void *load_scene(Stage *stage, char *name);
+void unload_all_scenes(void **handles);
+void **load_all_scenes(Stage *stage, char *directory);
 
 #endif
