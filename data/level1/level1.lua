7b04d39ae34b9cf6f599213f2b3f377e89d3585d
Thomas Meng <thomasmengmxc@gmail.com>
2018-07-11 12:57:56 +1000

    Changed some data files

--- data/level1/level1.lua
+++ data/level1/level1.lua
@@ -1,4 +1,30 @@
 return {
-	mode = "Rout",
-	map = "plain.lua",
+	win = function()
+		if Map:getCount("Enemies") == 0 then
+			return true
+		else
+			return false
+		end
+	end,
+	map = "forest.lua",
+	starting = {
+		{0, 0},
+		{0, 1},
+		{1, 0},
+		{1, 1},
+	},
+	builder = function()
+		local tmpUnit = {
+			name = "Grug",
+			icon = ";(",
+			hp = 10,
+			move = 5,
+			str = 5,
+			spd = 5,
+			def = 5,
+		}
+		Map:newFaction("Heroes")
+		Map:newFaction("Enemies")
+		Map:addUnit(tmpUnit, "Enemies", 3, 3)
+	end,
 }
